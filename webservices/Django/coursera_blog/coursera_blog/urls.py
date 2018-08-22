f86bdd8a43eed89eb21bf285a574d455a4658bbb
Alex <sashkaptits@gmail.com>
2018-08-22 17:32:38 +0300

    DJANGO

--- webservices/Django/coursera_blog/coursera_blog/urls.py
+++ webservices/Django/coursera_blog/coursera_blog/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
-    # path('index/', index),
+    path('index/', views.index),
     path('admin/', admin.site.urls),
 ]
