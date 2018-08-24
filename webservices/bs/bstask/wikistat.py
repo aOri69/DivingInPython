ef290bbc640458d4e3bfa6193a8706aa15347b19
Alex <sashkaptits@gmail.com>
2018-08-24 18:19:31 +0300

    BS Task

--- webservices/bs/bstask/wikistat.py
+++ webservices/bs/bstask/wikistat.py
@@ -3,28 +3,73 @@ import re
 import os
 
 
-def get_children(path: str, filename: str, files: dict) -> dict:
-    with open(f'{path}{filename}', encoding='UTF-8') as f:
-        link_re = re.compile(r"(?<=/wiki/)[\w()]+")  # Искать ссылки можно как угодно, не обязательно через re
-        children = {}
-        page = str(BeautifulSoup(f.read(), 'lxml').find('div', {'id': 'bodyContent'}))
-        links = link_re.findall(page)
-        del page
-        for link in links:
-            if link in files:
-                if not files.get(link) and link != filename:
-                    children[link] = filename  # Setting parent
+class Exists(Exception):
+    pass
+
+
+class Finder:
+
+    def __init__(self, start, end, path) -> None:
+        self.start = start
+        self.bridge = []
+        self.end = end
+        self.path = path
+        self.files = dict.fromkeys(os.listdir(path))
+        self.link_re = re.compile(r"(?<=/wiki/)[\w()]+")
+        self.__parsed_pages = set()
+
+    def find_path(self, start) -> list:
+        if self.end in self.bridge:
+            return self.bridge
+        children = self._get_links(start)
+        self.bridge.append(start)
+        if len(children) == 0:  # and self.end not in self.bridge:
+            self.bridge.pop(len(self.bridge) - 1)
+            return []
+        for child in children:
+            self.find_path(child)
+        return []
+
+    def _get_links(self, filename) -> list:
+        if filename in self.__parsed_pages:
+            return []
+        with open(f'{self.path}{filename}', encoding='UTF-8') as f:
+            children = []
+            page = str(BeautifulSoup(f.read(), 'lxml').find('div', {'id': 'bodyContent'}))
+            links = self.link_re.findall(page)
+            del page
+            self.__parsed_pages.add(filename)
+            for link in links:
+                if link in self.files:
+                    if not self.files.get(link) and link != filename:
+                        children.append(link)
         return children
+
+    # def get_children(path: str, filename: str, files: dict) -> dict:
+    #     with open(f'{path}{filename}', encoding='UTF-8') as f:
+    #         link_re = re.compile(r"(?<=/wiki/)[\w()]+")  # Искать ссылки можно как угодно, не обязательно через re
+    #         children = {}
+    #         page = str(BeautifulSoup(f.read(), 'lxml').find('div', {'id': 'bodyContent'}))
+    #         links = link_re.findall(page)
+    #         del page
+    #         for link in links:
+    #             if link in files:
+    #                 if not files.get(link) and link != filename:
+    #                     children[link] = filename  # Setting parent
+    #     return children
 
 
 # Вспомогательная функция, её наличие не обязательно и не будет проверяться
 def build_tree(start, end, path):
+    # link_re = re.compile(r"(?<=/wiki/)[\w()]+")  # Искать ссылки можно как угодно, не обязательно через re
     files = dict.fromkeys(os.listdir(path))  # Словарь вида {"filename1": None, "filename2": None, ...}
-    # TODO Проставить всем ключам в files правильного родителя в значение, начиная от start
-    page = start
-    lenght = 0
-    while page != end:
-        children = get_children(path, start, files)
+    # # TODO Проставить всем ключам в files правильного родителя в значение, начиная от start
+    # children = get_children(path, start, files)
+    # files.update(children)
+    # for child in children:
+    #     pass
+    finder = Finder(start, end, path)
+    brid = finder.find_path(start)
     return files
 
 
@@ -37,13 +82,6 @@ def build_bridge(start, end, path):
 
 
 def parse(start, end, path):
-    """
-    Если не получается найти список страниц bridge, через ссылки на которых можно добраться от start до end, то,
-    по крайней мере, известны сами start и end, и можно распарсить хотя бы их: bridge = [end, start]. Оценка за тест,
-    в этом случае, будет сильно снижена, но на минимальный проходной балл наберется, и тест будет пройден.
-    Чтобы получить максимальный балл, придется искать все страницы. Удачи!
-    """
-
     bridge = build_bridge(start, end, path)  # Искать список страниц можно как угодно, даже так: bridge = [end, start]
 
     # Когда есть список страниц, из них нужно вытащить данные и вернуть их
