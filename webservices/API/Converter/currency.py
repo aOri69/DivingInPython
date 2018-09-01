9b0ab4a6cb6f7ed665f13d8b7a070e8de90e976c
Alex <sashkaptits@gmail.com>
2018-09-01 10:34:08 +0300

    Конвертер валют + BS4 и Decimal Готово

--- webservices/API/Converter/currency.py
+++ webservices/API/Converter/currency.py
@@ -1,9 +1,15 @@
-from bs4 import BeautifulSoup
-from decimal import Decimal
+from bs4 import BeautifulSoup, Tag
+from decimal import Decimal, getcontext, localcontext
 
 
 def convert(amount, cur_from, cur_to, date, requests):
-    response = requests.get()  # Использовать переданный requests
-    # ...
-    result = Decimal('3754.8057')
+    url = 'http://www.cbr.ru/scripts/XML_daily.asp?'
+    if date is not None:
+        url += 'date_req='
+    response = requests.get(f'{url}{date}')  # Использовать переданный requests
+    soup = BeautifulSoup(response.content, 'xml')
+    tag_to = soup.find('CharCode', text=cur_to)
+    nominal = int(tag_to.find_next_sibling('Nominal').string)
+    value = Decimal(tag_to.find_next_sibling('Value').string.replace(',', '.', 1)) / nominal
+    result = Decimal(Decimal(amount) / value).quantize(Decimal('.0001'))
     return result  # не забыть про округление до 4х знаков после запятой
